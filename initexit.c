5f923016acc7611beadab526f54e7db9bef3731d
ravimalik <ravimalik836@gmail.com>
2016-05-25 22:52:30 +0530

    -Change the registration function

--- initexit.c
+++ initexit.c
@@ -9,8 +9,8 @@ dev_t dev;
 static int __init constructor(void)
 {
 	int ret;
-//	ret = alloc_chrdev_region(&dev,0,nod,"ravimodule");
-	ret = register_chrdev_region((dev_t)dev,nod,"ravimodule");
+	ret = alloc_chrdev_region(&dev,0,nod,"ravimodule");
+//	ret = register_chrdev_region((dev_t)dev,nod,"ravimodule");
 	if(ret==0)
 		pr_info("Hello, Module Registered\nMajor No. : %d\tminor : %d\n",MAJOR(dev),MINOR(dev));
 	return 0;
